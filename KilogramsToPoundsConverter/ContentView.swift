8f40095ca9beebeecd4fd22f490688fbc1ed0cf0
Mateo Elosua <melosua@lcs.on.ca>
2021-12-10 09:00:15 -0500

    Added a slider where you choose the mass you want to transform into a different unit

--- KilogramsToPoundsConverter/ContentView.swift
+++ KilogramsToPoundsConverter/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var massInKilograms = 0.0
+    
+    // MARK: Computed properties
+    var massInPounds: Double {
+        return massInKilograms * 2.205
+    }
+    var feedback: String {
+        switch  massInKilograms {
+        case (0...30):
+                return ""
+        case 30...60:
+                return ""
+            case 60...90:
+                return ""
+            case 90...120:
+                return ""
+            case 120...150:
+                return ""
+            default:
+                return ""
+        
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("Kilograms")
+            }
+            //MARK: Imput
+            Slider(value: $massInKilograms,
+                   in: 0.0...150.0,
+                   step: 0.1,
+                   label: {
+                Text("Kilograms")
+            },
+                   minimumValueLabel: {
+                Text("0")
+            },
+                   maximumValueLabel: {
+                Text("150")
+            })
+            
+            Text("\(String(format: "%.1f", massInKilograms)) Kg")
+                .bold()
+                .padding()
+            
+            HStack {
+                Text("Pounds")
+                    .bold()
+                
+                Spacer()
+            }
+            
+        }
     }
 }
 
