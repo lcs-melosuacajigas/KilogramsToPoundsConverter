04a539b8bed6c7f68e0c51fdcc147c72b92250d5
Mateo Elosua <melosua@lcs.on.ca>
2021-12-10 08:37:44 -0500

    Initial Commit

---
+++ KilogramsToPoundsConverter/KilogramsToPoundsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KilogramsToPoundsConverterApp.swift
+//  KilogramsToPoundsConverter
+//
+//  Created by Mateo Elosua on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct KilogramsToPoundsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
